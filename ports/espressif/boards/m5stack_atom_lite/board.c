c4d8629389743f79c416df04eb37761c7ae73b74
thandal <than@timbrel.org>
2024-05-16 17:27:25 -0400

    No pullup on 23 for inTENS

--- ports/espressif/boards/m5stack_atom_lite/board.c
+++ ports/espressif/boards/m5stack_atom_lite/board.c
@@ -26,4 +26,24 @@
 
 #include "supervisor/board.h"
 
+#include "shared-bindings/microcontroller/Pin.h"
+#include "components/driver/gpio/include/driver/gpio.h"
+
+bool espressif_board_reset_pin_number(gpio_num_t pin_number) {
+    // Pin 23 is PWM output to the voltage Boost/Cuk converter.
+    // We want to pull it low to turn off the input MOSFET.
+    if (pin_number == 23) {
+        gpio_config_t cfg = {
+            .pin_bit_mask = BIT64(pin_number),
+            .mode = GPIO_MODE_DISABLE,
+            .pull_up_en = false,
+            .pull_down_en = true,
+            .intr_type = GPIO_INTR_DISABLE,
+        };
+        gpio_config(&cfg);
+        return true;
+    }
+    return false;
+}
+
 // Use the MP_WEAK supervisor/shared/board.c versions of routines not defined here.
